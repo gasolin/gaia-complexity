3130e215f55b1f84c341ebd034595ae0fe5705c8
gasolin <gasolin@gmail.com>
2013-05-29 21:28:44 +0800

    change title

--- complexity.sh
+++ complexity.sh
@@ -3,26 +3,26 @@
 cd ..
 repo sync
 
-plato -d reports/bluetooth apps/bluetooth/js/*.js
-plato -d reports/browser apps/browser/js/*.js
-plato -d reports/calendar apps/calendar/js/*.js
-plato -d reports/camera apps/camera/js/*.js
-plato -d reports/clock apps/clock/js/*.js
+plato -d reports/bluetooth -t "Bluetooth Source Analysis" apps/bluetooth/js/*.js
+plato -d reports/browser -t "Browser Source Analysis" apps/browser/js/*.js
+plato -d reports/calendar -t "Calendar Source Analysis" apps/calendar/js/*.js
+plato -d reports/camera -t "Camera Source Analysis" apps/camera/js/*.js
+plato -d reports/clock -t "Clock Source Analysis" apps/clock/js/*.js
 
-plato -d reports/costcontrol apps/costcontrol/js/*.js
-plato -d reports/email apps/email/js/*.js
-plato -d reports/fm apps/fm/js/*.js
-plato -d reports/gallery apps/gallery/js/*.js
-plato -d reports/homescreen apps/homescreen/js/*.js
-plato -d reports/keyboard apps/keyboard/js/*.js
-plato -d reports/music apps/music/js/*.js
-plato -d reports/settings apps/settings/js/*.js
-plato -d reports/sms apps/sms/js/*.js
-plato -d reports/video apps/video/js/*.js
-plato -d reports/wallpaper apps/wallpaper/js/*.js
+plato -d reports/costcontrol -t "CostControl Source Analysis" apps/costcontrol/js/*.js
+plato -d reports/email -t "Email Source Analysis" apps/email/js/*.js
+plato -d reports/fm -t "FM Source Analysis" apps/fm/js/*.js
+plato -d reports/gallery -t "Gallery Source Analysis" apps/gallery/js/*.js
+plato -d reports/homescreen -t "Homescreen Source Analysis" apps/homescreen/js/*.js
+plato -d reports/keyboard -t "Keyboard Source Analysis" apps/keyboard/js/*.js
+plato -d reports/music -t "Music Source Analysis" apps/music/js/*.js
+plato -d reports/settings -t "Settings Source Analysis" apps/settings/js/*.js
+plato -d reports/sms -t "SMS Source Analysis" apps/sms/js/*.js
+plato -d reports/video -t "Video Source Analysis" apps/video/js/*.js
+plato -d reports/wallpaper -t "Wallpaper Source Analysis" apps/wallpaper/js/*.js
 
-plato -d reports/system apps/system/js/*.js
-plato -d reports/emergencycall apps/system/emergency-call/js/*.js
-plato -d reports/contacts apps/communications/contacts/js/*.js
-plato -d reports/dialer apps/communications/dialer/js/*.js
-plato -d reports/ftu apps/communications/ftu/js/*.js
+plato -d reports/system -t "System Source Analysis" apps/system/js/*.js
+plato -d reports/emergencycall -t "EmergencyCall Source Analysis" apps/system/emergency-call/js/*.js
+plato -d reports/contacts -t "Contacts Source Analysis" apps/communications/contacts/js/*.js
+plato -d reports/dialer -t "Dialer Source Analysis" apps/communications/dialer/js/*.js
+plato -d reports/ftu -t "FTU Source Analysis" apps/communications/ftu/js/*.js
